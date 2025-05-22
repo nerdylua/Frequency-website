c3ab94060682147fcb8889d4248b7d4198add548
Vidisha231106 <vidishadewan2020@gmail.com>
2025-05-22 23:46:07 +0530

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -86,29 +86,97 @@ const Header = () => {
       >
         <div className="container">
           <div className="relative flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-4">
+            {/* Logo at far left */}
+            <div className="w-40 max-w-full px-2 xl:mr-4 flex-shrink-0">
               <Link
                 href="/"
-                className={`header-logo block w-full ${
+                className={`header-logo block ${
                   sticky ? "py-5 lg:py-2" : "py-8"
-                } `}
+                }`}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
+                  src="/images/logo/vectorised_logo.svg"
                   alt="logo"
                   width={140}
-                  height={30}
-                  className="w-full dark:hidden"
+                  height={40}
+                  className="object-contain dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
+                  src="/images/logo/vectorised_logo_white.svg"
                   alt="logo"
                   width={140}
-                  height={30}
-                  className="hidden w-full dark:block"
+                  height={40}
+                  className="object-contain hidden dark:block"
                 />
               </Link>
             </div>
+            {/* Centered navbar */}
+            <div className="flex-1 flex justify-center">
+              <nav
+                id="navbarCollapse"
+                className={`navbar w-auto rounded border-[4px] border-body-color/50 bg-white px-3 py-1 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
+                  navbarOpen
+                    ? "absolute left-1/2 top-full z-30 -translate-x-1/2 visibility opacity-100"
+                    : "absolute left-1/2 top-[120%] z-30 -translate-x-1/2 invisible opacity-0"
+                } lg:relative lg:top-0 lg:left-0 lg:translate-x-0`}
+              >
+                <ul className="block lg:flex lg:space-x-8 xl:space-x-12">
+                  {menuData.map((menuItem, index) => (
+                    <li key={index} className="group relative">
+                      {menuItem.path ? (
+                        <Link
+                          href={menuItem.path}
+                          onClick={() => handleNavigation(menuItem.path)}
+                          className={`flex py-1 text-lg lg:mr-0 lg:inline-flex lg:px-0 lg:py-2 ${
+                            usePathName === menuItem.path
+                              ? "text-primary dark:text-white"
+                              : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
+                          }`}
+                        >
+                          {menuItem.title}
+                        </Link>
+                      ) : (
+                        <>
+                          <p
+                            onClick={() => handleSubmenu(index)}
+                            className="flex cursor-pointer items-center justify-between py-2 text-lg text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
+                          >
+                            {menuItem.title}
+                            <span className="pl-3">
+                              <svg width="25" height="24" viewBox="0 0 25 24">
+                                <path
+                                  fillRule="evenodd"
+                                  clipRule="evenodd"
+                                  d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                                  fill="currentColor"
+                                />
+                              </svg>
+                            </span>
+                          </p>
+                          <div
+                            className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
+                              openIndex === index ? "block" : "hidden"
+                            }`}
+                          >
+                            {menuItem.submenu?.map((submenuItem, index) => (
+                              <Link
+                                href={submenuItem.path}
+                                key={index}
+                                onClick={() => handleNavigation(submenuItem.path)}
+                                className="block rounded py-2.5 text-base text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3"
+                              >
+                                {submenuItem.title}
+                              </Link>
+                            ))}
+                          </div>
+                        </>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            {/* Right side: theme toggler and mobile menu */}
             <div className="flex items-center gap-4">
               <div className="lg:hidden">
                 <ThemeToggler />
@@ -135,71 +203,6 @@ const Header = () => {
                   }`}
                 />
               </button>
-            </div>
-            <nav
-              id="navbarCollapse"
-              className={`navbar absolute right-0 top-full z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
-                navbarOpen
-                  ? "visibility top-full opacity-100"
-                  : "invisible top-[120%] opacity-0"
-              }`}
-            >
-              <ul className="block lg:flex lg:space-x-8 xl:space-x-12">
-                {menuData.map((menuItem, index) => (
-                  <li key={index} className="group relative">
-                    {menuItem.path ? (
-                      <Link
-                        href={menuItem.path}
-                        onClick={() => handleNavigation(menuItem.path)}
-                        className={`flex py-2 text-lg lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
-                          usePathName === menuItem.path
-                            ? "text-primary dark:text-white"
-                            : "text-dark hover:text-primary dark:text-white/70 dark:hover:text-white"
-                        }`}
-                      >
-                        {menuItem.title}
-                      </Link>
-                    ) : (
-                      <>
-                        <p
-                          onClick={() => handleSubmenu(index)}
-                          className="flex cursor-pointer items-center justify-between py-2 text-lg text-dark group-hover:text-primary dark:text-white/70 dark:group-hover:text-white lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
-                        >
-                          {menuItem.title}
-                          <span className="pl-3">
-                            <svg width="25" height="24" viewBox="0 0 25 24">
-                              <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
-                                fill="currentColor"
-                              />
-                            </svg>
-                          </span>
-                        </p>
-                        <div
-                          className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
-                            openIndex === index ? "block" : "hidden"
-                          }`}
-                        >
-                          {menuItem.submenu?.map((submenuItem, index) => (
-                            <Link
-                              href={submenuItem.path}
-                              key={index}
-                              onClick={() => handleNavigation(submenuItem.path)}
-                              className="block rounded py-2.5 text-base text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3"
-                            >
-                              {submenuItem.title}
-                            </Link>
-                          ))}
-                        </div>
-                      </>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </nav>
-            <div className="flex items-center justify-end pr-4 lg:pr-0">
               <div className="hidden lg:block">
                 <ThemeToggler />
               </div>
