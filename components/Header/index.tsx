49e40f6688bd3f41df2bf106adf6fd37a12d2e45
nerdylua <nihaalsprakash.is23@rvce.edu.in>
2025-05-24 16:11:32 +0530

    Refactor Header component for improved styling and layout

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -7,12 +7,12 @@ import ThemeToggler from "./ThemeToggler";
 import menuData from "./menuData";
 
 // Create a new component for the menu
-const NavigationMenu = ({ 
-  menuData, 
-  usePathName, 
-  handleNavigation, 
-  handleSubmenu, 
-  openIndex 
+const NavigationMenu = ({
+  menuData,
+  usePathName,
+  handleNavigation,
+  handleSubmenu,
+  openIndex
 }) => {
   return (
     <ul className="block lg:flex lg:space-x-8 xl:space-x-12">
@@ -49,7 +49,7 @@ const NavigationMenu = ({
                 </span>
               </p>
               <div
-                className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
+                className={`submenu absolute right-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:left-0 lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
                   openIndex === index ? "block" : "hidden"
                 }`}
               >
@@ -138,21 +138,21 @@ const Header = () => {
         }`}
         style={{
           transform: sticky
-            ? `translateY(${scrollProgress * 3}px) scale(${
-                1 - scrollProgress * 0.08
+            ? `translateY(${scrollProgress * 4}px) scale(${
+                1 - scrollProgress * 0.12
               })`
             : "none",
-          borderRadius: sticky ? `${scrollProgress * 20}px` : "0",
-          margin: sticky ? `${scrollProgress * 1.5}px auto` : "0 auto",
-          maxWidth: sticky ? `calc(100% - ${scrollProgress * 40}px)` : "100%",
+          borderRadius: sticky ? `${scrollProgress * 30}px` : "0",
+          margin: sticky ? `${scrollProgress * 2}px auto` : "0 auto",
+          maxWidth: sticky ? `calc(100% - ${scrollProgress * 60}px)` : "100%",
           boxShadow: sticky
-            ? `0 ${scrollProgress * 8}px ${scrollProgress * 16}px rgba(0, 0, 0, 0.1)`
+            ? `0 ${scrollProgress * 10}px ${scrollProgress * 20}px rgba(0, 0, 0, 0.1)`
             : "none",
         }}
       >
         <div className="container">
           <div className="relative flex items-center justify-between py-1.5">
-            <div className="w-16 max-w-full px-1 xl:mr-4 lg:w-20">
+            <div className="w-24 max-w-full px-1 xl:mr-4">
               <Link
                 href="/"
                 className={`header-logo block `}
@@ -160,25 +160,25 @@ const Header = () => {
                 <Image
                   src="/images/logo/white.svg"
                   alt="logo"
-                  width={6}
-                  height={6}
-                  className="w-full dark:hidden rounded-md lg:w-20"
+                  width={8}
+                  height={8}
+                  className="w-full dark:hidden rounded-md"
                 />
                 <Image
                   src="/images/logo/dark.svg"
                   alt="logo"
-                  width={6}
-                  height={6}
-                  className="hidden w-full dark:block rounded-md lg:w-20"
+                  width={8}
+                  height={8}
+                  className="hidden w-full dark:block rounded-md"
                 />
               </Link>
             </div>
             <div className="flex-1 flex justify-center">
               <nav
                 id="navbarCollapse"
-                className={`navbar w-auto rounded border-[3px] border-body-color/50 bg-white px-2.5 py-0.5 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
+                className={`navbar w-auto rounded border-[4px] border-body-color/50 bg-white px-3 py-1 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
                   navbarOpen
-                    ? "absolute left-1/2 top-full z-30 -translate-x-1/2 visibility opacity-100"
+                    ? "absolute top-full right-0 z-30 visibility opacity-100"
                     : "absolute left-1/2 top-[120%] z-30 -translate-x-1/2 invisible opacity-0"
                 } lg:relative lg:top-0 lg:left-0 lg:translate-x-0`}
               >
@@ -191,7 +191,7 @@ const Header = () => {
                 />
               </nav>
             </div>
-            <div className="flex items-center gap-3">
+            <div className="flex items-center gap-4">
               <div className="lg:hidden">
                 <ThemeToggler />
               </div>
@@ -199,26 +199,24 @@ const Header = () => {
                 onClick={navbarToggleHandler}
                 id="navbarToggler"
                 aria-label="Mobile Menu"
-                className="block rounded-lg px-2.5 py-[3px] ring-primary focus:ring-2 lg:hidden"
+                className="block rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
               >
                 <span
-                  className={`relative my-1 block h-0.5 w-[25px] bg-black transition-all duration-300 dark:bg-white ${
-                    navbarOpen ? " top-[6px] rotate-45" : " "
+                  className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
+                    navbarOpen ? " top-[7px] rotate-45" : " "
                   }`}
                 />
                 <span
-                  className={`relative my-1 block h-0.5 w-[25px] bg-black transition-all duration-300 dark:bg-white ${
+                  className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
                     navbarOpen ? "opacity-0 " : " "
                   }`}
                 />
                 <span
-                  className={`relative my-1 block h-0.5 w-[25px] bg-black transition-all duration-300 dark:bg-white ${
-                    navbarOpen ? " top-[-7px] -rotate-45" : " "
+                  className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
+                    navbarOpen ? " top-[-8px] -rotate-45" : " "
                   }`}
                 />
               </button>
-            </div>
-            <div className="flex items-center justify-end pr-3 lg:pr-0">
               <div className="hidden lg:block">
                 <ThemeToggler />
               </div>
